f15f5ea9c84684916c5e86133d9bc242d6db3ab8
htetsoemoe <soemoehtet.mdy@gmail.com>
2023-10-15 11:44:04 +0630

    updated with yml file

--- 1.spring-boot-starting/src/main/java/com/ninja/spring/controller/HelloController.java
+++ 1.spring-boot-starting/src/main/java/com/ninja/spring/controller/HelloController.java
@@ -1,14 +1,18 @@
 package com.ninja.spring.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 	
+	@Value("${app.message}")
+	private String message;
+	
 	@GetMapping("/")
 	public String index() {
-		return "Hello from 10 hours long Spring Boot Master Class!";
+		return message;
 	}
 
 }
