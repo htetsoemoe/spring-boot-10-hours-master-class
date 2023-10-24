f38aa42595690b38d7a7ac6e3c8903ccd303f5da
htetsoemoe <soemoehtet.mdy@gmail.com>
2023-10-24 11:02:48 +0630

    OneToManay relationship with teacher and course

---
+++ 2.spring-boot-data-jpa/src/test/java/com/ninja/spring/repotest/TeacherRepoTest.java
@@ -0,0 +1,41 @@
+package com.ninja.spring.repotest;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.ninja.spring.entity.Course;
+import com.ninja.spring.entity.Teacher;
+import com.ninja.spring.repository.TeacherRepository;
+
+@SpringBootTest
+public class TeacherRepoTest {
+	
+	@Autowired
+	private TeacherRepository teacherRepository;
+	
+	@Test
+	public void saveTeacher() {
+			
+		Course courseJava = Course.builder()
+				.courseName("Java")
+				.credit(7)
+				.build();
+		
+		Course courseDBA = Course.builder()
+				.courseName("DBA")
+				.credit(7)
+				.build();
+		
+		Teacher teacher = Teacher.builder()
+				.firstName("Ko")
+				.lastName("Htet")
+				.courses(List.of(courseJava, courseDBA))
+				.build();
+		
+		teacherRepository.save(teacher);
+	}
+
+}
